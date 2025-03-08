db8e8fe6bb45e27f077e065a270a84c71155c7c3
Shubham <shubhamsaini8965@gmail.com>
2025-03-08 16:20:56 +0530

    created navbar little bit more responsive

--- src/components/Ui/Navbar.jsx
+++ src/components/Ui/Navbar.jsx
@@ -15,14 +15,14 @@ const Navbar = () => {
                 <h1 className='text-[28px]  font-lilita-one font-semibold italic'><Link to={'/'}>World Atlas</Link></h1>
 
                 <button
-                    className='block sm:hidden p-2 focus:outline-none'
+                    className='block md:hidden p-2 focus:outline-none'
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
                 >
                     <RxHamburgerMenu size={25} />
                 </button>   
 
-                <div className={`${isMenuOpen ? 'block shadow bg-white'  : 'hidden'} absolute top-16 left-0 w-full dark:bg-[#202020] p-4 sm:static sm:flex sm:items-center  sm:w-auto rounded-b-3xl  sm:rounded-none z-10 `}>
-                    <ul className=' flex sm:flex-row  flex-col items-end list-none sm:gap-10 gap-5 sm:px-0 px-4 text-[25px] sm:text-[20px] mt-2.5 sm:mt-0'>
+                <div className={`${isMenuOpen ? 'block shadow bg-white'  : 'hidden'} absolute top-16 left-0 w-full dark:bg-[#202020] p-4 md:static md:flex sm:items-center md:w-auto rounded-b-3xl  md:rounded-none z-10 `}>
+                    <ul className=' flex md:flex-row  flex-col items-end list-none md:gap-8 gap-5 md:px-0 px-4 text-[25px] md:text-[20px] mt-2.5 md:mt-0'>
                         <li className='hover:text-[#0D6EFD] hover:underline' onClick={() => setIsMenuOpen(false)}><Link to={'/'}>Home</Link> </li>
                         <li className='hover:text-[#0D6EFD] hover:underline' onClick={() => setIsMenuOpen(false)}><Link to={'/about'}>About</Link> </li>
                         <li className='hover:text-[#0D6EFD] hover:underline' onClick={() => setIsMenuOpen(false)}><Link to={'/country'}>Country</Link> </li>
